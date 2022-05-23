47398489e62be2fcc458b1ef8d461a52253daa47
Alberto <alberto.tavares@zup.com.br>
2022-05-23 10:04:52 -0300

    definindo user agent api java

--- apis-canais-digitais/templates-spring-open-feign/{{inputs.caminho_geracao_arquivos}}/CanaisDigitais.java
+++ apis-canais-digitais/templates-spring-open-feign/{{inputs.caminho_geracao_arquivos}}/CanaisDigitais.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import feign.Headers;
 
 @FeignClient(url = "https://canaisdigitais-api.localiza.com/al-canaisdigitais-site/v1/", name = "canais-digitais")
-@Headers("client_id:{{inputs.client_id}}")
+@Headers({"client_id:{{inputs.client_id}}","user-agent:stackspot"})
 public interface CanaisDigitais {
 
 	@GetMapping("/Agencias/localidade/{cidade}")
